f674f0670d8291fb6809c3c084b904c1f9926261
Adebayo Abdulwaasi <wasiuadekunle73@gmail.com>
2021-08-11 17:38:22 +0100

    modal/sidebar app completed

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Home from './Home';
+import Modal from './Modal';
+import Sidebar from './Sidebar';
+
+const App = () => {
+  return (
+    <div>
+      <Home/>
+      <Sidebar/>
+      <Modal/>
+    </div>
+  )
+}
+
+export default App
